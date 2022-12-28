febb1aa43d7655c2dd192782fc051a621180f0e4
Mratunjya Shakhdhar <mratunjya13@gmail.com>
2022-12-28 20:21:07 +0530

    routing update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,14 @@
 import Head from "next/head";
-import PDF from "@common/pdf";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Home = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.isReady) router.push("/MCA/Sem-4/Syllabus");
+  }, [router]);
+
   return (
     <>
       <Head>
@@ -10,9 +17,8 @@ const Home = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <PDF />
     </>
   );
-}
+};
 
 export default Home;
